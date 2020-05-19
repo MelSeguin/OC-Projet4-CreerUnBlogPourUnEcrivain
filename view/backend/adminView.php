d6f885e13c58498df3e20793f7ac94c48fb0cd6a
Mélanie <melseguin@gmail.com>
2020-05-19 23:31:36 +0200

    Update adminView.php

--- view/backend/adminView.php
+++ view/backend/adminView.php
@@ -2,34 +2,18 @@
 
 <?php ob_start(); ?>
 
-<div class="navbar">
-  <a class="navlink" href="index.php?action=listPosts"> VOIR MON BLOG </a>
-  <a class="navlink" href="index.php?action=newPost"> NOUVEL ARTICLE </a>
-  <a class="navlink" href="index.php?action=displayFlags">
-    <?php if($getNumber > 0) : ?>
-      <i class="fas fa-flag red"></i>
-    <?php else :?>
-      <i class="fas fa-flag grey"></i>
-    <?php endif; ?>
-    <?php echo $getNumber[0];?></a>
-  <a class="navlink" href="index.php?action=logout"> DECONNEXION </a>
-</div>
+  <div class="navbar">
+    <a class="navlink" href="index.php?action=listPosts"> <i class="fas fa-feather-alt"></i>  VOIR MON BLOG </a>
+    <a class="navlink" href="index.php?action=newPost"><i class="fas fa-book-open"></i> NOUVEL ARTICLE </a>
+    <a class="navlink" href="index.php?action=displayFlags"><i class="fas fa-flag"></i>  <?php echo $getNumber[0];?></a>
+    <a class="navlink" href="index.php?action=logout"> <i class="fas fa-sign-out-alt"></i>  DECONNEXION </a>
+  </div>
 
+  <p class="admin-subtitle"> Bonjour <?php echo $_SESSION['name']?>, bienvenu sur votre tableau de bord. <br/>
+    Prêt pour rédiger un <a class='text-link' href='index.php?action=newPost'> nouveau chapitre</a> ?</p>
+  <hr>
 
- <h1> Billet simple pour l'Alaska</h1>
-
- <p class="subtitle">
-  <?php
-    if (isset($_SESSION['name'])) {
-      echo $welcomeMessage;
-    } else {
-      echo $errorMessage;
-    }
-   ?>
-   <br/>
- </p>
-
- <table>
+ <table class="admin-listposts">
     <tr>
         <th>TOUS LES ARTICLES</th>
         <th>EN LIGNE</th>
@@ -40,7 +24,7 @@
     <tr>
       <?php while($posts = $getPosts -> fetch()):?>
         <?php if ($posts): ?>
-          <td><a href="index.php?action=post&amp;id=<?= $posts[0] ?>"><?php echo htmlspecialchars($posts[1]); ?></a></td><!--afficher le titre de l'article-->
+          <td><a class ="admin-listposts-links" href="index.php?action=post&amp;id=<?= $posts[0] ?>"><?php echo htmlspecialchars($posts[1]); ?></a></td><!--afficher le titre de l'article-->
           <td><?php if ($posts[4] == 'yes') :?> <!--si l'article est publié-->
               <p class="green-circle"><i class="far fa-check-circle"></i></p> <!--afficher une pastille verte-->
               <?php else :?>
