123a6662339f7b3d378959dfccedc87c5a47c4c4
Mélanie <melseguin@gmail.com>
2020-05-20 12:22:04 +0200

    mise à jour css
    intégration du design pour les pages :
    - createPostView
    - updatePostView

--- view/backend/createPostView.php
+++ view/backend/createPostView.php
@@ -1,26 +1,29 @@
-<?php $title = "Créer un nouvel article | Billet simple pour l'Alaska"; ?>
+<?php $title = "Nouvel Article | Billet simple pour l'Alaska"; ?>
 
 <?php ob_start(); ?>
 
-<h1 > Billet simple pour l'Alaska </h1>
-<p class ="subtitle"> Créer un nouvel article <i class="far fa-keyboard"></i> </p>
-<hr>
-<?php
-if (isset($savePost) && $savePost == false){
-      echo " Votre article n'a pas pu être enregistré. Merci de rééssayer plus tard. <br/>";
-  }
-?>
-<form action="index.php?action=savePost" method="post">
-  <label for="title">Titre : </label>
+<div class="edit-post-container">
+  <h2> Nouvel article </h2>
+    <hr>
+  <?php
+    if (isset($savePost) && $savePost == false){
+      echo " <p> Votre article n'a pas pu être enregistré. Merci de rééssayer plus tard.</p>";
+    }
+  ?>
+    <form action="index.php?action=savePost" method="post">
+      <i class="fas fa-book-open"></i> <input type="text" name="title" value="" placeholder="Titre de l'article" required>
+      <input type="checkbox" name="published" value="" >
+      <label for="published"> PUBLIER </label>
+      <input type="submit" name="submit" value="ENREGISTRER">
+      <?php if(isset($_SESSION['name'])) : ?>
+        <a href="index.php?action=admin"> RETOUR </a>
+      <?php else : ?>
+        <a href="index.php?action=listPosts"> RETOUR </a>
+      <?php endif; ?>
+      <textarea id="content" name="content" required>  </textarea>
+    </form>
+</div>
 
-  <input type="text" name="title" value="" placeholder=" Mon titre ici..." required>
-  <textarea id="content" name="content" required>  </textarea>
-
-  <label for="published"> PUBLIER ?</label>
-  <input type="checkbox" name="published" value="yes" checked>
-  <input type="submit" name="submit" value="ENREGISTRER">
-  <a href="index.php?action=listPosts"> FERMER </a>
-</form>
 
 <?php $content = ob_get_clean(); ?>
 
--- view/backend/updatePostView.php
+++ view/backend/updatePostView.php
@@ -1,24 +1,18 @@
-<?php $title = "Mettre à jour un article | Billet simple pour l'Alaska"; ?>
+<?php $title = "Editer | Billet simple pour l'Alaska"; ?>
 
 <?php ob_start(); ?>
-
-<h1 > Billet simple pour l'Alaska </h1>
-<p class ="subtitle"> Mettre à jour un article </p>
-<hr>
-
-
-<form action="index.php?action=updatePost&amp;id=<?= $editPost[0] ?>" method="post">
-  <label for="title">Titre : </label>
-
-  <input type="text" name="title" value="<?php echo $editPost['post_title'] ?>" required>
-  <textarea id="content" name="content" required> <?php echo $editPost['post_content'] ?>  </textarea>
-
-  <label for="published"> PUBLIER ?</label>
-  <input type="checkbox" name="published" value="yes" checked>
-  <input type="submit" name="submit" value="ENREGISTRER">
-  <a href="index.php?action=displayAdminView"> FERMER </a>
-</form>
-
+<div class="edit-post-container">
+  <h2> Edition de l'article </h2>
+    <hr>
+    <form action="index.php?action=updatePost&amp;id=<?= $editPost[0] ?>" method="post">
+      <i class="fas fa-book-open"></i> <input type="text" name="title" value="<?php echo $editPost['post_title'] ?>" required>
+      <input type="checkbox" name="published" value="" >
+      <label for="published"> PUBLIER </label>
+      <input type="submit" name="submit" value="ENREGISTRER">
+      <a href="index.php?action=admin"> FERMER </a>
+      <textarea id="content" name="content" required> <?php echo $editPost['post_content'] ?>  </textarea>
+    </form>
+</div>
 <?php $content = ob_get_clean(); ?>
 
 <?php require('view/template.php'); ?>
